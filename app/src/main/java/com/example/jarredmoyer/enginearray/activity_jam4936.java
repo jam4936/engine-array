0b4ee4b65b216467ee5a6139a2ebb8243a1fd988
DESKTOP-7B4UMH3\Jarred Moyer <jam4936@rit.edu>
2018-10-02 14:54:50 -0400

    Created personal activity

---
+++ app/src/main/java/com/example/jarredmoyer/enginearray/activity_jam4936.java
@@ -0,0 +1,13 @@
+package com.example.jarredmoyer.enginearray;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_jam4936 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jam4936);
+    }
+}
