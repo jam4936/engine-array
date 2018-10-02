56d48d313f2cf8528f05dded2bc61b2bb079eee7
Aaron Jiang <aj6566@rit.edu>
2018-10-02 14:55:23 -0400

    AaronJ Tutorial4

---
+++ app/src/main/java/com/example/jarredmoyer/enginearray/AaronJiangTutorial4.java
@@ -0,0 +1,13 @@
+package com.example.jarredmoyer.enginearray;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AaronJiangTutorial4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aaron_jiang_tutorial4);
+    }
+}
